63b6d94e870d55ac30858e32c8472b8b96ecbd39
Jason-Gregoire <61832961+Jason-Gregoire@users.noreply.github.com>
2023-07-10 16:58:54 -0400

    Create Navbar.js

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="logo">Wrixle</div>
+      <ul className="nav-links">
+        <li>Home</li>
+        <li>About</li>
+        <li>Portfolio</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
