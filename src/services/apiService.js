6273bb6dda003a202f27ab1e108801bc6f2ae0ba
Jason-Gregoire <61832961+Jason-Gregoire@users.noreply.github.com>
2023-07-10 16:17:17 -0400

    Update apiService.js

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,1 +1,46 @@
 // This file can handle API integrations with external data sources. It can include functions for fetching data from third-party APIs, such as market data providers or blockchain explorers
+
+// Function to make an API request to fetch portfolio data
+export const fetchPortfolioData = async (userId) => {
+  try {
+    const response = await fetch(`/api/portfolio/${userId}`);
+    if (!response.ok) {
+      throw new Error('Failed to fetch portfolio data');
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Failed to fetch portfolio data:', error);
+    return null;
+  }
+};
+
+// Function to make an API request to update portfolio data
+export const updatePortfolioData = async (userId, portfolioData) => {
+  try {
+    const response = await fetch(`/api/portfolio/${userId}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(portfolioData),
+    });
+    if (!response.ok) {
+      throw new Error('Failed to update portfolio data');
+    }
+    const updatedData = await response.json();
+    return updatedData;
+  } catch (error) {
+    console.error('Failed to update portfolio data:', error);
+    return null;
+  }
+};
+
+// Other API-related functions...
+
+// Export the apiService object with the defined functions
+export default {
+  fetchPortfolioData,
+  updatePortfolioData,
+  // Other functions...
+};
