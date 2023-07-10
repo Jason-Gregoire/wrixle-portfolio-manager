93cdac5aac64ac68ccf47b87ad2d3c3e00182bb1
Jason-Gregoire <61832961+Jason-Gregoire@users.noreply.github.com>
2023-07-10 16:14:50 -0400

    Update ethereumService.js

--- src/services/ethereumService.js
+++ src/services/ethereumService.js
@@ -1,1 +1,52 @@
-//This file can contain functions and logic related to interacting with the Ethereum network, such as fetching token balances, transaction data, or other on-chain information
+//This file can contain functions and logic related to interacting with the Ethereum network, such as fetching token balances, transaction data, or other on-chain information.
+
+// Import required libraries
+import Web3 from 'web3';
+
+// Initialize Web3 provider
+const web3 = new Web3(window.ethereum);
+
+// Function to check if the Ethereum network is connected
+export const checkNetworkConnection = async () => {
+  try {
+    await web3.eth.net.getId();
+    return true;
+  } catch (error) {
+    console.error('Failed to connect to Ethereum network:', error);
+    return false;
+  }
+};
+
+// Function to fetch token balance for a specific address
+export const getTokenBalance = async (tokenAddress, walletAddress) => {
+  try {
+    const contract = new web3.eth.Contract(tokenAbi, tokenAddress);
+    const balance = await contract.methods.balanceOf(walletAddress).call();
+    return balance;
+  } catch (error) {
+    console.error('Failed to fetch token balance:', error);
+    return null;
+  }
+};
+
+// Function to fetch transaction history for a specific address
+export const getTransactionHistory = async (walletAddress) => {
+  try {
+    const history = await web3.eth.getTransactionsByAddress(walletAddress);
+    return history;
+  } catch (error) {
+    console.error('Failed to fetch transaction history:', error);
+    return null;
+  }
+};
+
+// Other Ethereum-related functions...
+
+// Export the ethereumService object with the defined functions
+export default {
+  checkNetworkConnection,
+  getTokenBalance,
+  getTransactionHistory,
+  // Other functions...
+};
+
