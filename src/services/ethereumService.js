268f17d1db67face3c9cf715e4e94fe998ac1f26
Jason-Gregoire <61832961+Jason-Gregoire@users.noreply.github.com>
2023-07-10 16:10:36 -0400

    Update ethereumService.js

--- src/services/ethereumService.js
+++ src/services/ethereumService.js
@@ -1,1 +1,1 @@
-holder
+//This file can contain functions and logic related to interacting with the Ethereum network, such as fetching token balances, transaction data, or other on-chain information
