3564dbe93b10fd676eba26ac769f50e906e5b321
Jason-Gregoire <61832961+Jason-Gregoire@users.noreply.github.com>
2023-07-10 17:00:38 -0400

    Create Fotter.js

---
+++ src/components/Fotter.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <p>© 2023 Wrixle. All rights reserved.</p>
+        <ul className="footer-links">
+          <li>Terms of Service</li>
+          <li>Privacy Policy</li>
+          <li>Contact Us</li>
+        </ul>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
