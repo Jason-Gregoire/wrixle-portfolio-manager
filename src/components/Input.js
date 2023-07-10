2002f5c99010ceae05d236801128ff7e171610f8
Jason-Gregoire <61832961+Jason-Gregoire@users.noreply.github.com>
2023-07-10 16:43:26 -0400

    Create Input.js

---
+++ src/components/Input.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Input.css';
+
+const Input = ({ type, placeholder, value, onChange }) => {
+  return (
+    <input
+      type={type}
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange}
+      className="input"
+    />
+  );
+};
+
+Input.propTypes = {
+  type: PropTypes.string.isRequired,
+  placeholder: PropTypes.string,
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
+export default Input;
