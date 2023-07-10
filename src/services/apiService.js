4f1c82d1c746e0f9b1be4bb8a369c82198a54a16
Jason-Gregoire <61832961+Jason-Gregoire@users.noreply.github.com>
2023-07-10 16:11:17 -0400

    Create apiService.js

---
+++ src/services/apiService.js
@@ -0,0 +1,1 @@
+// This file can handle API integrations with external data sources. It can include functions for fetching data from third-party APIs, such as market data providers or blockchain explorers
