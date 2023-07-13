e4257af2994605ba211dd1922741b5342fdbefd9
Jason-Gregoire <61832961+Jason-Gregoire@users.noreply.github.com>
2023-07-13 07:31:51 -0400

    Update logger.js

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,1 +1,16 @@
+// This file can handle logging and debugging functionalities.
+// logger.js
 
+// Helper function to log messages to the console with timestamps
+export function logMessage(message) {
+  const timestamp = new Date().toISOString();
+  console.log(`[${timestamp}] ${message}`);
+}
+
+// Helper function to log error messages to the console with timestamps
+export function logError(error) {
+  const timestamp = new Date().toISOString();
+  console.error(`[${timestamp}] ${error}`);
+}
+
+// Other logging utility functions as needed
