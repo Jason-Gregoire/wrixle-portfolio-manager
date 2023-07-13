6efc3c25b305d0c5fd0e71948059603f2215e0a5
Jason-Gregoire <61832961+Jason-Gregoire@users.noreply.github.com>
2023-07-13 07:33:40 -0400

    Update validation.js

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,1 +1,22 @@
+// This file can include functions for data validation and input sanitization.
+// validation.js
 
+// Helper function to validate an email address
+export function validateEmail(email) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
+
+// Helper function to validate a password
+export function validatePassword(password) {
+  // Add your password validation logic here
+  return password.length >= 8;
+}
+
+// Helper function to validate a name
+export function validateName(name) {
+  // Add your name validation logic here
+  return name.length > 0;
+}
+
+// Other validation utility functions as needed
