590c3fb1d2ca5337c4f5ddee516f949ef137d7b4
Jason-Gregoire <61832961+Jason-Gregoire@users.noreply.github.com>
2023-07-13 07:24:01 -0400

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,17 @@
+// This file can define constants and configuration settings used throughout the application.
+// constants.js
 
+// API endpoint URLs
+export const API_BASE_URL = 'https://api.wrixle.com';
+export const API_LOGIN_URL = `${API_BASE_URL}/login`;
+export const API_PORTFOLIOS_URL = `${API_BASE_URL}/portfolios`;
+export const API_ASSETS_URL = `${API_BASE_URL}/assets`;
+
+// Local storage keys
+export const LOCAL_STORAGE_ACCESS_TOKEN_KEY = 'accessToken';
+
+// Error messages
+export const ERROR_LOGIN_FAILED = 'Login failed. Please check your credentials and try again.';
+export const ERROR_LOGIN_GENERIC = 'An error occurred during login. Please try again later.';
+
+// Other constants as needed
