055cc153ae2b035fc49ab24c828df7cfdff91157
Jason-Gregoire <61832961+Jason-Gregoire@users.noreply.github.com>
2023-07-13 07:27:05 -0400

    Update date.js

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,1 +1,27 @@
+// This file can include functions for date manipulation and formatting.
+// date.js
 
+// Format a date in a specific format
+export function formatDate(date, format = 'YYYY-MM-DD') {
+  const formattedDate = new Intl.DateTimeFormat('en-US', { dateStyle: format }).format(date);
+  return formattedDate;
+}
+
+// Get the current date and time
+export function getCurrentDateTime() {
+  const currentDate = new Date();
+  const currentDateTime = new Intl.DateTimeFormat('en-US', {
+    dateStyle: 'medium',
+    timeStyle: 'short',
+  }).format(currentDate);
+  return currentDateTime;
+}
+
+// Calculate the difference between two dates
+export function calculateDateDifference(date1, date2) {
+  const diffInMilliseconds = Math.abs(date2 - date1);
+  const diffInDays = Math.floor(diffInMilliseconds / (1000 * 60 * 60 * 24));
+  return diffInDays;
+}
+
+// Other date utility functions as needed
