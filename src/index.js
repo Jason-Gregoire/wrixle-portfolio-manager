4cd79fed1bd1e26d961da190296275c8a4d783a0
Jason-Gregoire <61832961+Jason-Gregoire@users.noreply.github.com>
2023-07-13 08:47:48 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-holder - mtf
+// responsible for rendering the root component of the application and mounting it to the DOM.
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
