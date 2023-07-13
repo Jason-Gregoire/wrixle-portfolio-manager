cd656bbd2a8512c2828b0be2a806c092c19606d0
Jason-Gregoire <61832961+Jason-Gregoire@users.noreply.github.com>
2023-07-13 07:30:41 -0400

    Update helpers.js

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,1 +1,21 @@
+// This file can store helper functions that provide common functionality across the application.
+// helpers.js
 
+// Helper function to calculate the average value of an array
+export function calculateAverage(array) {
+  if (!Array.isArray(array) || array.length === 0) {
+    return 0;
+  }
+
+  const sum = array.reduce((acc, val) => acc + val, 0);
+  const average = sum / array.length;
+
+  return average;
+}
+
+// Helper function to generate a random number within a specified range
+export function generateRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Other helper utility functions as needed
