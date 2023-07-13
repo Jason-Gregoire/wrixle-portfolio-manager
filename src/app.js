ec7f4aae66187368db043f1312a6d47f8a7691a4
Jason-Gregoire <61832961+Jason-Gregoire@users.noreply.github.com>
2023-07-13 08:57:24 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,30 @@
+// sets up the routing and structure of your application by defining the routes and rendering the appropriate components based on the URL
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
+import PortfolioDashboard from './pages/PortfolioDashboard';
+import AssetExplorer from './pages/AssetExplorer';
+import TradingPlatform from './pages/TradingPlatform';
+import NewsInsights from './pages/NewsInsights';
+import AccountSettings from './pages/AccountSettings';
+import SupportHelpCenter from './pages/SupportHelpCenter';
+import NotFound from './components/NotFound';
 
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={LandingPage} />
+        <Route path="/dashboard" component={PortfolioDashboard} />
+        <Route path="/assets" component={AssetExplorer} />
+        <Route path="/trading" component={TradingPlatform} />
+        <Route path="/news" component={NewsInsights} />
+        <Route path="/account" component={AccountSettings} />
+        <Route path="/support" component={SupportHelpCenter} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
