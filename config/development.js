8ff9501e8dd3ed19816bec24b7429f7ac03f405f
Jason-Gregoire <61832961+Jason-Gregoire@users.noreply.github.com>
2023-07-14 06:43:54 -0400

    Update development.js

--- config/development.js
+++ config/development.js
@@ -1,1 +1,14 @@
-holder
+const config = {
+  environment: 'development',
+  apiUrl: 'https://api.example.com',
+  apiKey: 'YOUR_DEVELOPMENT_API_KEY',
+  database: {
+    host: 'localhost',
+    port: 5432,
+    username: 'dev_user',
+    password: 'dev_password',
+    database: 'dev_database',
+  },
+};
+
+module.exports = config;
