c171c2b8de9228f7894d81a6709e999bad176d61
Jason-Gregoire <61832961+Jason-Gregoire@users.noreply.github.com>
2023-07-14 06:45:47 -0400

    Update production.js

--- config/production.js
+++ config/production.js
@@ -1,1 +1,26 @@
-holder
+module.exports = {
+  // Database Configuration
+  database: {
+    host: 'production-db.example.com',
+    port: 5432,
+    username: 'your-username',
+    password: 'your-password',
+    database: 'wrixle_production',
+  },
+
+  // API Configuration
+  api: {
+    baseURL: 'https://api.wrixle.com',
+    timeout: 5000,
+    apiKey: 'your-api-key',
+  },
+
+  // Logging Configuration
+  logging: {
+    level: 'info',
+    file: '/var/log/wrixle.log',
+  },
+
+  // Other Configuration Options
+  // ...
+};
