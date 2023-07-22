8f84a196502cc8f922fc10fe2cc0ab095031db76
Jason-Gregoire <61832961+Jason-Gregoire@users.noreply.github.com>
2023-07-21 20:55:24 -0500

    Update NotFound.js

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -3,7 +3,7 @@ import './NotFound.css';
 
 function NotFound() {
   return (
-    <div>
+    <div className="not-found-container"> {/* Add the missing class */}
       <h1>404 - Page Not Found</h1>
       <p>The page you're looking for does not exist.</p>
     </div>
